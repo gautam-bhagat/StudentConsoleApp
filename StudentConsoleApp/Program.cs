6fef8b14d457f274198d4fe23cd615410c52df07
mrgautambhagat <73634938+mrgautambhagat@users.noreply.github.com>
2021-06-11 16:13:46 +0530

    Initial commit

---
+++ StudentConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentConsoleApp
+{
+    class Student {
+        public String rollno, fname, lnam;
+    }
+    class Marks : Student {
+        float eng, mar, hin, maths, sst, sci;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marks m = new Marks();
+            Console.Write("Enter Roll no. : ");
+            m.rollno = Console.ReadLine();
+            Console.WriteLine(m.rollno);
+            Console.ReadKey();
+
+        }
+    }
+}
